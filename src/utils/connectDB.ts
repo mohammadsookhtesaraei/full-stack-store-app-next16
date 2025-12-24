a2f198f4bb2de2895cc0a21f44a3776b6d28dd7b
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-12-24 13:28:18 +0330

    added connectDb function for connect to database

---
+++ src/utils/connectDB.ts
@@ -0,0 +1,36 @@
+
+
+
+
+import mongoose from "mongoose";
+
+
+const mongoURL=process.env.MONGODB_URL!
+
+
+
+ async function connectDB(){
+ try{
+
+ if(mongoose.connection.readyState===1){
+    console.log("Already connected to MongoDB!");
+    return
+ }
+
+ mongoose.set("strictQuery",false);
+ 
+ await mongoose.connect(mongoURL);
+ console.log("connectDB!");
+
+ }catch(error:unknown){
+  if(error instanceof Error){
+    console.log(error.message);
+     throw error;
+  }
+   throw new Error("Unknown error while connecting to MongoDB");
+ }
+};
+
+
+
+export default connectDB;
